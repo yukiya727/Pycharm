33b527becd105a3583fec350285963c8750fa1c5
yukiya727 <yukiya727p@gmail.com>
2020-07-06 01:42:25 +0800

    commit

--- Thread record.py
+++ Thread record.py
@@ -1,49 +1,158 @@
 import threading
-import mouse
-import keyboard
+import time
+import os
+import sys
+from pynput import mouse
+from pynput import keyboard
 
-mouse_events = []
-keyboard_events = []
 
-keyboard.wait(".")
+# import mouse
+# import keyboard
 
-mouse.hook(mouse_events.append)
-keyboard.start_recording()
+# mouse_events = []
+# keyboard_events = []
+#
+# keyboard.wait(".")
+#
+# mouse.hook(mouse_events.append)
+# keyboard.hook_key(keyboard_events.append)
+# # keyboard.start_recording()
+#
+# keyboard.wait(".")
+#
+# mouse.unhook(mouse_events.append)
+# keyboard.unhook(keyboard_events.append)
+#
+# # keyboard_events = keyboard.stop_recording()
+#
+# for key in keyboard_events:
+#     temp = str(key)
+#     temp = temp.replace("("," ")
+#     temp = temp.replace(")", "")
+#     res = temp.split(" ")
+#     print(res)
+#     # print("\n")
+#
+# for key in mouse_events:
+#     temp = str(key)
+#     if temp.find("MoveEvent") != -1:
+#         continue
+#     temp = temp.replace("("," ")
+#     temp = temp.replace(")", "")
+#     res = temp.split(" ")
+#     print(res)
+#     # print("\n")
 
-keyboard.wait(".")
-
-mouse.unhook(mouse_events.append)
-keyboard_events = keyboard.stop_recording()
-
-for key in keyboard_events:
-    temp = str(key)
-    temp = temp.replace("("," ")
-    temp = temp.replace(")", "")
-    res = temp.split(" ")
-    print(res)
-    # print("\n")
-
-for key in mouse_events:
-    temp = str(key)
-    if temp.find("MoveEvent") != -1:
-        continue
-    temp = temp.replace("("," ")
-    temp = temp.replace(")", "")
-    res = temp.split(" ")
-    print(res)
-    # print("\n")
-
-#Keyboard threadings:
+# Keyboard threadings:
 
 # k_thread = threading.Thread(target = lambda :keyboard.play(keyboard_events))
 # k_thread.start()
 
-#Mouse threadings:
+# Mouse threadings:
 
 # m_thread = threading.Thread(target = lambda :mouse.play(mouse_events))
 # m_thread.start()
 
-#waiting for both threadings to be completed
+# waiting for both threadings to be completed
 
 # k_thread.join()
 # m_thread.join()
+
+def on_move(x, y):
+    global file_path
+    print('MouseMove,{0},{1},{2}'.format(
+        x, y, time.time()))
+    with open(file_path, 'a') as f:
+        f.write('MouseMove,{0},{1},{2}'.format(x, y, time.time()))
+        f.write('\n')
+
+
+def on_click(x, y, button, pressed):
+    global file_path
+    print('{0},{1},{2},{3},{4}'.format(
+        'MousePressed' if pressed else 'MouseReleased', button, x, y, time.time()))
+    with open(file_path, 'a') as f:
+        f.write('{0},{1},{2},{3},{4}'.format(
+            'MousePressed' if pressed else 'MouseReleased', button, x, y, time.time()))
+        f.write('\n')
+    # if not pressed:
+    #     # Stop listener
+    #     return False
+
+
+def on_scroll(x, y, dx, dy):
+    global file_path
+    print('MouseScrolled,{0},{1},{2}'.format(
+        x, y, time.time()))
+    with open(file_path, 'a') as f:
+        f.write('MouseScrolled,{0},{1},{2}'.format(
+            x, y, time.time()))
+        f.write('\n')
+
+
+def on_press(key):
+    global file_path
+    try:
+        print('KeyPressed,{0},{1}'.format(
+            key.char, time.time()))
+        with open(file_path, 'a') as f:
+            f.write('KeyPressed,{0},{1}'.format(
+                key.char, time.time()))
+            f.write('\n')
+    except AttributeError:
+        print('KeyPressed,{0},{1}'.format(
+            key, time.time()))
+        with open(file_path, 'a') as f:
+            f.write('KeyPressed,{0},{1}'.format(
+                key, time.time()))
+            f.write('\n')
+
+
+def on_release(key):
+    global file_path
+    print('KeyReleased,{0},{1}'.format(
+        key, time.time()))
+    with open(file_path, 'a') as f:
+        f.write('KeyReleased,{0},{1}'.format(
+            key, time.time()))
+        f.write('\n')
+
+    if key == keyboard.Key.f10:
+        with open(file_path, 'a') as f:
+            f.write('done')
+            f.write('\n')
+        # Stop listener
+        return False
+    if key == keyboard.Key.f9:
+        with open(file_path, 'a') as f:
+            f.write('start')
+            f.write('\n')
+
+
+# keyboard.wait(".")
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+directory = 'mouse_recorder'
+try:
+    session_name = "pynput_record"
+except:
+    print('you must enter a name for the session\nfor example: python record.py session_name')
+    sys.exit()
+dir_path = os.path.join(os.getcwd(), directory, session_name)
+if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
+file_name = 'history.txt'
+file_path = os.path.join(dir_path, file_name)
+print(dir_path)
+
+# kl = keyboard.Listener(target = on_press, arg =(key=None))
+
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release
+        ) as k_listener, \
+        mouse.Listener(
+        on_move=on_move,
+        on_click=on_click,
+        on_scroll=on_scroll) as m_listener:
+    k_listener.join()
+    m_listener.join()
--- replay.py
+++ replay.py
@@ -2,63 +2,119 @@
 import time
 import os
 import sys
-import pyautogui
+import keyboard
+import pydirectinput
+import pydirectinput
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 directory = 'mouse_recorder'
 try:
-    session_name = "python_record"
+    session_name = "pynput_record"
 except:
-    print 'you must enter a name for the session\nfor example: python replay.py session_name'
+    print('you must enter a name for the session\nfor example: python replay.py session_name')
     sys.exit()
 dir_path = os.path.join(os.getcwd(), directory, session_name)
 
 file_name = 'history.txt'
 file_path = os.path.join(dir_path, file_name)
-#print dir_path
+# print(dir_path)
 
 # open the recording file
 with open(file_path, 'r') as f:
     steps = f.readlines()
 
+
+
 # clean steps
 new_steps = []
+temp_step = []
+t_last = 0.0
+t_temp = 0.0
+id = 0
+startcount = False
+
 for step in steps:
     new_step = []
     for i in step.split(','):
+        if step.find("Key.") != -1:
+            step.replace("Key.", "")
         new_step.append(i.strip('\n'))
-    new_steps.append(new_step)
 
+    if startcount != True:
+        if new_step[0] != 'start' and new_step[0] != 'done':
+            t_last = float(new_step[-1])
+        if new_step[0] == 'start':
+            startcount = True
+        continue
+
+    if new_step[0] == 'MouseMove' and id != 0:
+        if new_steps[id-1][0] == 'MouseMove':
+            if float(new_step[-1]) - t_last < 0.1:
+                temp_step = new_step
+                t_temp = float(new_step[-1])
+                continue
+        t_last = float(new_step[-1])
+
+    elif new_step[0] != 'MouseMove' and id > 1:
+        if new_steps[id - 1][0] == 'MouseMove' and new_steps[id - 2][0] == 'MouseMove':
+            new_steps.append(temp_step)
+            t_last = t_temp
+
+
+    new_steps.append(new_step)
+    id += 1
+
+print("Recorded objects:", len(new_steps))
 
 # start moving mouse cursor
 t_last = float(new_steps[0][-1])
 
+print("Press F9 to replay")
+keyboard.wait("F9")
+
 for step in new_steps:
 
-    if step[0] == 'left_down':
-        time.sleep(float(step[-1]) - t_last)
-        pyautogui.moveTo(int(step[1]), int(step[2]))
-        pyautogui.mouseDown(button='left')
-        t_last = float(step[-1])
 
-    if step[0] == 'left_up':
+    if step[0] == 'MousePressed' and step[1] == 'Button.left':
         time.sleep(float(step[-1]) - t_last)
-        pyautogui.moveTo(int(step[1]), int(step[2]))
-        pyautogui.mouseUp(button='left')
+        pydirectinput.moveTo(int(step[2]), int(step[3]))
+        pydirectinput.mouseDown(button='left')
         t_last = float(step[-1])
+        continue
 
-    if step[0] == 'right_down':
+    if step[0] == 'MouseReleased' and step[1] == 'Button.left':
         time.sleep(float(step[-1]) - t_last)
-        pyautogui.moveTo(int(step[1]), int(step[2]))
-        pyautogui.mouseDown(button='right')
+        pydirectinput.moveTo(int(step[2]), int(step[3]))
+        pydirectinput.mouseUp(button='left')
         t_last = float(step[-1])
+        continue
 
-    if step[0] == 'right_up':
+    if step[0] == 'MousePressed' and step[1] == 'Button.right':
         time.sleep(float(step[-1]) - t_last)
-        pyautogui.moveTo(int(step[1]), int(step[2]))
-        pyautogui.mouseUp(button='right')
+        pydirectinput.moveTo(int(step[2]), int(step[3]))
+        pydirectinput.mouseDown(button='right')
         t_last = float(step[-1])
+        continue
+
+    if step[0] == 'MouseReleased' and step[1] == 'Button.right':
+        time.sleep(float(step[-1]) - t_last)
+        pydirectinput.moveTo(int(step[2]), int(step[3]))
+        pydirectinput.mouseUp(button='right')
+        t_last = float(step[-1])
+        continue
+
+    if step[0] == 'KeyPressed':
+        time.sleep(float(step[-1]) - t_last)
+        pydirectinput.keyDown(step[1])
+        t_last = float(step[-1])
+        continue
+
+    if step[0] == 'KeyReleased':
+        time.sleep(float(step[-1]) - t_last)
+        pydirectinput.keyUp(step[1])
+        t_last = float(step[-1])
+        continue
 
     if step[0] == 'done':
-        print 'End autorun'
+        print('End autorun')
         sys.exit()
